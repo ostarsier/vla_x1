ed37ee0e7060b83a2a297cfead6d2b6c71f15be0
shelbin <ostarsier@163.com>
2025-05-06 14:37:53 +0800

    init

---
+++ vla_x1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'vla_x1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yonsvm',
+    maintainer_email='yonsvm@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'topic_jointstate_sub = vla_x1.topic_jointstate_sub:main',
+            'topic_helloworld_pub  = vla_x1.topic_helloworld_pub:main',
+            'topic_helloworld_sub  = vla_x1.topic_helloworld_sub:main',
+        ],
+    },
+)
